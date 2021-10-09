89643739126c3fb4215a3757c6cacc803f41a8f8
Rangel Stoilov <rangelstoilov@gmail.com>
2021-10-09 16:36:24 +0300

    return full data

--- src/seaport.ts
+++ src/seaport.ts
@@ -3029,7 +3029,7 @@ export class OpenSeaPort {
   console.log('_dispatch event match orders')
   this._dispatch(EventType.MatchOrders, { buy, sell, accountAddress, matchMetadata: metadata })
 
-  const txnData: any = { from: accountAddress, value }
+  const txnData: any = { from: accountAddress, value: value?.toString(), to: buy.exchange }
   const args: WyvernAtomicMatchParameters = [
     [buy.exchange, buy.maker, buy.taker, buy.feeRecipient, buy.target,
     buy.staticTarget, buy.paymentToken, sell.exchange, sell.maker, sell.taker, sell.feeRecipient, sell.target, sell.staticTarget, sell.paymentToken],
